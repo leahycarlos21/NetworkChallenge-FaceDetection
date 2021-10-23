5b2ff93fca5aa2888fee6b228793e1e797e95b13
Fabelifer2797 <fabelifer@gmail.com>
2021-10-23 13:33:30 -0600

    [ADD] Implementing QThreads in the UI application

--- Client/clientGUI.py
+++ Client/clientGUI.py
@@ -1,14 +1,16 @@
 from ftplib import FTP
 from PyQt5.uic import loadUi
 from PyQt5 import QtGui, QtWidgets
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtWidgets import QDialog,QFileDialog, QApplication,QScrollArea
 import sys
 import os
 import requests
 import shutil 
+import time
 
 class ApplicationScreen(QDialog):
+    
 
     def __init__(self):
 
@@ -20,9 +22,12 @@ class ApplicationScreen(QDialog):
         self.jCounter = 0
         self.ftp = None
         self.btn_sell = None
+        self.btn_sell2 = None
         self.setColumnWidthTables()
         self.selectButton.clicked.connect(self.openFileExplorer)
         self.uploadButton.clicked.connect(self.uploadFile)
+        
+
 
     def setFTP(self, newFtp):
         self.ftp = newFtp
@@ -36,7 +41,11 @@ class ApplicationScreen(QDialog):
         filename = self.currentFileName 
         #self.ftp.storbinary('STOR '+filename, open(filename, 'rb'))
         #self.ftp.quit()
-        self.imageTable.setItem(self.iCounter - 1,self.jCounter + 1,QtWidgets.QTableWidgetItem("Image Uploaded...")) 
+        self.imageTable.setItem(self.iCounter - 1,self.jCounter + 2,QtWidgets.QTableWidgetItem("Image Uploaded..."))
+        self.cfThread = checkFileThread()
+        self.cfThread.started.connect(self.checkFile)
+        self.cfThread.start()
+
 
     def setCurrentPath(self, currentPath):
         self.pathLabel.setText(currentPath)
@@ -47,16 +56,17 @@ class ApplicationScreen(QDialog):
         self.currentFileName = pathList[-1]
         self.setRowCountTables(self.iCounter + 1)
         self.imageTable.setItem(self.iCounter,self.jCounter,QtWidgets.QTableWidgetItem(self.currentFileName))
-        self.imageTable.setItem(self.iCounter,self.jCounter + 1,QtWidgets.QTableWidgetItem(""))
+        self.imageTable.setItem(self.iCounter,self.jCounter + 2,QtWidgets.QTableWidgetItem(""))
         self.btn_sell = QtWidgets.QPushButton('Show')
         self.btn_sell.clicked.connect(self.handlerButton)
-        self.imageTable.setCellWidget(self.iCounter, self.jCounter + 2, self.btn_sell)
+        self.imageTable.setCellWidget(self.iCounter, self.jCounter + 1, self.btn_sell)
         self.iCounter += 1
 
     def setColumnWidthTables(self):
         self.imageTable.setColumnWidth(0,300)
         self.imageTable.setColumnWidth(1,300)
         self.imageTable.setColumnWidth(2,300)
+        self.imageTable.setColumnWidth(3,300)
 
     def setRowCountTables(self, rowNumber):
         
@@ -71,6 +81,45 @@ class ApplicationScreen(QDialog):
             image = QtGui.QImage('./' + fileName)
             pixmap = QtGui.QPixmap.fromImage(image)
             self.imageLabel.setPixmap(pixmap.scaled(461, 321, Qt.KeepAspectRatio))
+            
+    def handlerButton2(self):
+    
+        button = self.sender()
+        index = self.imageTable.indexAt(button.pos())
+        if index.isValid():
+            fileName = self.imageTable.item(index.row(),0).text()
+            CompleteFileName = "processed-" + fileName
+            image = QtGui.QImage('./' + CompleteFileName)
+            pixmap = QtGui.QPixmap.fromImage(image)
+            self.imageLabel.setPixmap(pixmap.scaled(461, 321, Qt.KeepAspectRatio))
+            
+    def checkFile(self):
+        
+        status = False
+        
+        while not status :
+            time.sleep(2)
+            status = True
+            
+        self.imageTable.setItem(self.iCounter - 1,self.jCounter + 2,QtWidgets.QTableWidgetItem("Image Ready!"))
+        self.btn_sell2 = QtWidgets.QPushButton('Show')
+        self.btn_sell2.clicked.connect(self.handlerButton2)
+        self.imageTable.setCellWidget(self.iCounter - 1, self.jCounter + 3, self.btn_sell2)
+        self.cfThread.stopThread()
+        
+ 
+class checkFileThread(QThread):
+     
+    def __init__(self):
+         QThread.__init__(self)
+         
+         
+    def stopThread(self):
+        self.terminate()       
+    
+            
+            
+            
         
 
 
